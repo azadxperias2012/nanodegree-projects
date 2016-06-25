e8376cfaf5c621ce4c5c2701840ef68989fb75ad
azadxperias2012 <azadxperias2012@gmail.com>
2016-06-25 08:34:17 +0530

    Optional Task : ProgressBar Added

---
+++ BuildItBigger/FinalProject/app/src/androidTestPaid/java/com/udacity/gradle/builditbigger/PaidEndpointsAsyncTaskTest.java
@@ -0,0 +1,28 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.test.AndroidTestCase;
+import android.util.Pair;
+
+import com.example.Joke;
+
+/**
+ * Created by aabbasal on 6/20/2016.
+ */
+public class PaidEndpointsAsyncTaskTest extends AndroidTestCase {
+
+    public void testApp() {
+        final String joke = Joke.tellJokes();
+        assertNotNull(joke);
+        new EndpointsAsyncTask() {
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                assertNotNull(result);
+                assertEquals("Should be same as the joke", joke, result);
+            }
+        }.execute(new Pair<Context, String>(this.getContext(), joke));
+
+    }
+
+}
